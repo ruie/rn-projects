b9ad30a7514dc85952edbbc17d10b0a38745e725
pragmaticbot <seocored@gmail.com>
2017-03-07 22:38:25 +0800

    Initial commit for Simple Album

---
+++ simple-album/main.js
@@ -0,0 +1,16 @@
+'use strict'
+
+import Exponent from 'exponent';
+import React from 'react';
+import {
+  Text
+} from 'react-native';
+
+const App = () => {
+  return (
+    <Text>Sample Text</Text>
+  );
+}
+
+Exponent.registerRootComponent(App);
+// AppRegister.registerComponent('simple-album', () => App);
