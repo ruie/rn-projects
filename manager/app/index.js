45625f0cbce979c06aec46df7805fec656bc08d7
pragmaticbot <seocored@gmail.com>
2017-03-14 23:28:21 +0800

    Created Manager app

---
+++ manager/app/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+        <Text>Hello</Text>
+      </View>
+    );
+  }
+}
+
+export default App;
