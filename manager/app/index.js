f7b54949929734773abb08f15be03456389dbb18
pragmaticbot <seocored@gmail.com>
2017-03-16 22:53:53 +0800

    Installed redux, react-redux

--- manager/app/index.js
+++ manager/app/index.js
@@ -1,13 +1,20 @@
 import React, { Component } from 'react';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import {
   View,
   Text
 } from 'react-native';
 
+const store = createStore(Reducer);
+
 class App extends Component {
   render() {
     return (
-      <View>Sample</View>
+      <Provider store={store}>
+        <View style={{ flex: 1 }}>Sample</View>
+      </Provider>
     );
   }
 }
