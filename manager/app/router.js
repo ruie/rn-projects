d2c610f292afbe1c7b6384074f8ca2d14a945e14
pragmaticbot <seocored@gmail.com>
2017-03-24 23:40:29 +0800

    Added fb authentication

--- manager/app/router.js
+++ manager/app/router.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Scene, Router, Actions } from 'react-native-router-flux';
+import firebase from 'firebase';
 
 import LoginScreen from './components/loginScreen';
 import EmployeeListScreen from './components/employeeListScreen';
@@ -9,7 +10,7 @@ const RouterComponent = () => {
   return (
     <Router>
       <Scene key={'auth'} >
-        <Scene key={'login'} component={LoginScreen} title={'Auth'} />
+        <Scene key={'login'} component={LoginScreen} title={'Auth'} hideNavBar/>
       </Scene>
 
       <Scene key={'main'}>
@@ -19,6 +20,8 @@ const RouterComponent = () => {
           component={EmployeeListScreen}
           title={'List'}
           rightTitle={'Add'}
+          leftTitle={'Signout'}
+          onLeft={() => firebase.auth().signOut()}
           initial
         />
         <Scene
---
+++ stubu/main.js
@@ -0,0 +1,51 @@
+import Expo from 'expo';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import { Button } from 'react-native-elements';
+
+class App extends React.Component {
+
+  authenticate = (token) => {
+    const provider = firebase.auth.FacebookAuthProvider;
+    const credential = provider.credential(token);
+    return firebase.auth().signInWithCredential(credential);
+  }
+
+  login = async () => {
+	const ADD_ID = 273131576444313
+	const options = {
+		permissions: ['public_profile', 'email'],
+	}
+	const {type, token} = await Expo.Facebook.logInWithReadPermissionsAsync(ADD_ID, options)
+	if (type === 'success') {
+		const response = await fetch(`https://graph.facebook.com/me?access_token=${token}`)
+		console.log(await response.json());
+		this.authenticate(token);
+	}
+}
+
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up main.js to start working on your app!</Text>
+        <Button
+          raised
+          onPress={this.login}
+          icon={{name: 'cached'}}
+          title='RAISED WITH ICON' />
+      </View>
+    );
+  }
+
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+Expo.registerRootComponent(App);
