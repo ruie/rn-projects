1f47204d9c0811fd420530d2ca5d1b4eaf3ce90c
pragmaticbot <seocored@gmail.com>
2017-03-31 20:32:27 +0800

    Updated bottom-nav app

---
+++ bottom-nav/app/index.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import {
+  View,
+  Text
+} from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>
+          Sample text here wh
+        </Text>
+      </View>
+    );
+  }
+}
+
+export default App;
