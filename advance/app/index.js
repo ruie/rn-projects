f8698fb14ab035d2f5e6b4c2cc14fb29f12c0f1f
pragmaticbot <seocored@gmail.com>
2017-04-30 22:14:11 +0800

    Initialize advance app

---
+++ advance/app/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+
+class App extends React.Component {
+
+  render() {
+    return (
+      <View>
+        <Text>
+          Ruie
+        </Text>
+      </View>
+    );
+  }
+
+}
+
+export default App;
